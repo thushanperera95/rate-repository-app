6236cb87fb325a3de66468abc0d4a5a88b6938ed
Thushan <thushan.perera95@gmail.com>
2022-06-16 17:38:23 +0800

    Apollo client uri now stored in environment variables.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client/react';
+
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
 
